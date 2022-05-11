b4904d509a6fbed37fac77b78d0adef96e85d1c0
Daniel Interiano <dannyboyint@gmail.com>
2022-05-11 16:29:49 -0400

    Downloaded react select for review component

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './Reviews.css';
 
-export default function Reviews() {
-  return (
-    <div>Reviews</div>
-  )
+export default function Reviews({ reviews, setReviews, revform, handleChange }) {
+
+    const filteredReviews = reviews?.filter((review) => {
+        if (search === "All") return true;
+        return review.score === search;
+    });
+    function handleDelete(id) {
+        fetch(`http://localhost:4000/reviews/${id}`, {
+            method: "DELETE",
+        }).then((res) => {
+            if (res.ok) {
+                const updatedReviewsList = reviews?.filter((review) => {
+                    return review.id !== id
+                });
+                setReviews(updatedReviewsList)
+            }
+        });
+    }
+    return (
+        <div>
+            <h2>Anime Reviews</h2>
+
+        </div>
+    )
 }
